9093ccf640a65fe3e20330198b6b106698294590
wjh <wjh201216@foxmail.com>
2024-11-07 17:35:11 +0800

    修改

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
 import Home from '../Home/index.vue'
 import Detail from '../Detail/index.vue'
 
@@ -16,7 +16,7 @@ const routes = [
 ]
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes
 })
 
